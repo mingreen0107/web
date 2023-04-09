2afcb31f553c50e95c6c150f8ca05185d0df05bb
mingreen0107 <71gnos@naver.com>
2023-04-09 23:56:02 +0900

    실습

---
+++ Project1/css4.c
@@ -0,0 +1,22 @@
+/*<!DOCTYPE html>
+<html lang="ko">
+    <head>
+        <meta charset="UTF-8">
+        <title>bottom</title>
+        <style>
+            p {
+                padding: 20px;
+                background-color: #000000;
+                color: #ffff00;
+            }
+        </style>
+    </head>
+    <body>
+        <p>È«±æµ¿ ±¤°í ¸ñ·Ï</p>
+    </body>
+</html>*/
+#include <stdio.h>
+
+int main() {
+    printf("css ass 1");
+}
