5ccf4fe12f2ea54e2b51656a841cb12349c941a5
mingreen0107 <71gnos@naver.com>
2023-04-09 23:57:47 +0900

    실습

---
+++ Project1/css6.c
@@ -0,0 +1,36 @@
+/*<!DOCTYPE html>
+<html lang="ko">
+    <head>
+        <meta charset="UTF-8">
+        <title>center</title>
+        <style>
+            table {
+              border:2px solid black;
+              border-collapse:collapse;
+            }
+            td, th {
+              border:2px solid black;
+              width: 200px;
+              text-align: center;
+            }
+          </style>
+    </head>
+    <body>
+        <table>
+            <tbody>
+                <tr>
+                    <td colspan="2">홍길동 육상 종목</td>
+                </tr>
+                <tr>
+                    <td><a href="#">트랙종목</a></td>
+                    <td><a href="#">필드종목</a></td>
+                </tr>
+            </tbody>
+        </table>
+    </body>
+</html>*/
+#include <stdio.h>
+
+int main() {
+    printf("css ass 1");
+}
