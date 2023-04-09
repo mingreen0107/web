d4ebe0b72508ca42d269a44d05d544fd442636d4
mingreen0107 <71gnos@naver.com>
2023-04-10 00:48:02 +0900

    실습

---
+++ Project1/css7.c
@@ -0,0 +1,17 @@
+/*p {
+    background: linear-gradient(90deg, red 20%, green 40% 60%, blue 80%);
+    color: #fff;
+    border:5px double;
+    width:300px;
+    padding: thin;
+    border-radius: 10px;
+    border-width: 10px 10px;
+    box-sizing: border-box;
+    font-size: 20px;
+    text-align: center;
+}*/
+#include <stdio.h>
+
+int main() {
+    printf("css ass 1");
+}
