d056de0eed5d88ab7a7127dd7bb272497dbb5001
mingreen0107 <71gnos@naver.com>
2023-04-09 23:54:54 +0900

    실습

---
+++ Project1/css3.c
@@ -0,0 +1,24 @@
+/*<!DOCTYPE html>
+<html lang="ko">
+    <head>
+        <meta charset="UTF-8">
+        <title>css_1</title>
+    </head>
+    <body>
+        <div class="introduce">
+            <div id="name">
+                <p style="font-size: 20px; width: 300px;
+                box-sizing: border-box; border: 5px double;
+                border-radius: 10px; border-width: 10px 10px;
+                color: #ffffff; background:
+                linear-gradient(to right,#ff0000,#00ff00,#0000ff);
+                text-align: center;">컴퓨터공학과 송민주</p>
+            </div>
+        </div>
+    </body>
+</html>*/
+#include <stdio.h>
+
+int main() {
+    printf("css ass 1");
+}
