c44691efd4fc4d4fd281873fc0c8d0b697dcd025
mingreen0107 <71gnos@naver.com>
2023-04-09 23:53:51 +0900

    실습

---
+++ Project1/css2.c
@@ -0,0 +1,62 @@
+/*<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <title>고급 선택자</title>
+  <style>
+   div {
+		width:150pt;
+		}
+	.contents h3 {
+		background-color: yellow;
+		border-style: dashed;
+		border-color: red;
+		border-width: 3px;
+	}
+	.contents li:nth-child(odd), div li:nth-child(2n+1) {
+		background-color: #00ff37;
+		color: red;
+		border-style: solid;
+		border-color: black;
+		border-width: thin;
+	}
+  </style>
+</head>
+<body>
+  
+  <div class="contents">
+	<span>
+    <h3>야구</h3>
+		<ul>
+			<li>MLB
+			<li>KBO
+			<li>JBL
+			<li>MXB
+		</ul>
+		<hr color=blue>
+    <h3>축구</h3>
+		<ul>
+			<li>프리미어리그
+			<li>라리가
+			<li>분데스리가
+			<li>리그앙
+		</ul>
+		<hr color=blue>
+	<h3>골프	</h3>
+		<ul>
+			<li>PGA
+			<li>LPGA
+			<li>KPGA
+			<li>ELP
+		</ul>
+		<hr color=blue>
+	</span>
+  </div>
+
+</body>
+</html>*/
+#include <stdio.h>
+
+int main() {
+	printf("css ass 1");
+}
