2455f8f75c0ac69daaff033deb8b4646a5dfa530
mingreen0107 <71gnos@naver.com>
2023-04-09 23:56:53 +0900

    실습

---
+++ Project1/css5.c
@@ -0,0 +1,25 @@
+/*<!DOCTYPE html>
+<html lang="ko">
+    <head>
+        <meta charset="UTF-8">
+        <title>top</title>
+        <style>
+            * {
+                margin: 0;
+            }
+            p {
+                font-size: large;
+                padding: 40px;
+                color: #fff;
+            }
+        </style>
+    </head>
+    <body>
+        <marquee behavior = "alternate" bgcolor="0000ff"><p>홍길동 올림픽/아시안게임</p></marquee>
+    </body>
+</html>*/
+#include <stdio.h>
+
+int main() {
+    printf("css ass 1");
+}
