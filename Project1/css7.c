172518e0002efdd362c3486cf4881b69fa17694b
mingreen0107 <71gnos@naver.com>
2023-04-11 22:28:22 +0900

    실습

--- Project1/css7.c
+++ Project1/css7.c
@@ -1,15 +1,21 @@
-/*p {
-    background: linear-gradient(90deg, red 20%, green 40% 60%, blue 80%);
-    color: #fff;
-    border:5px double;
-    width:300px;
-    padding: thin;
-    border-radius: 10px;
-    border-width: 10px 10px;
-    box-sizing: border-box;
-    font-size: 20px;
-    text-align: center;
-}*/
+/*<!DOCTYPE html>
+<html lang="ko">
+    <head>
+        <meta charset="UTF-8">
+        <title>default</title>
+    </head>
+    <body>
+        <iframe src="./start/top.html" align="top"
+        title="top" width="100%" height="100%"></iframe>
+        <iframe src="./start/left.html" align="left"
+        title="로고" width="15%" height="500px"></iframe>
+        <iframe src="./start/center.html" align="center"
+        title="로고" width="68%" height="500px"></iframe>
+        <iframe src="./start/right.html" align="right"
+        title="개최지" width="15%" height="500px"></iframe>
+    </body>
+</html>
+*/
 #include <stdio.h>
 
 int main() {
