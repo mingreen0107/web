87e52f14f6d1f5eff04d5d5fe3c0ec2aaad02373
mingreen0107 <71gnos@naver.com>
2023-04-11 22:29:33 +0900

    실습

--- Project1/css3.c
+++ Project1/css3.c
@@ -1,21 +1,16 @@
 /*<!DOCTYPE html>
 <html lang="ko">
-    <head>
-        <meta charset="UTF-8">
-        <title>css_1</title>
-    </head>
-    <body>
-        <div class="introduce">
-            <div id="name">
-                <p style="font-size: 20px; width: 300px;
-                box-sizing: border-box; border: 5px double;
-                border-radius: 10px; border-width: 10px 10px;
-                color: #ffffff; background:
-                linear-gradient(to right,#ff0000,#00ff00,#0000ff);
-                text-align: center;">컴퓨터공학과 송민주</p>
-            </div>
-        </div>
-    </body>
+<head>
+    <style>
+        p {
+            font-size: 30px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body bgcolor=#88ff00>
+    <p><b>트랙종목 게시판</b></p>
+</body>
 </html>*/
 #include <stdio.h>
 
