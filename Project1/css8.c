fdf4df6c2dcb2d3c9266216491084354a5883b0f
mingreen0107 <71gnos@naver.com>
2023-05-07 14:25:47 +0900

    복습

---
+++ Project1/css8.c
@@ -0,0 +1,39 @@
+/*<!DOCTYPE html>
+<html lang="ko">
+    <head>
+        <meta charset="UTF-8">
+        <title>center</title>
+        <style>
+            table {
+              border:2px solid black;
+              border-collapse:collapse;
+            }
+            td, th {
+              border:2px solid black;
+              width: 200px;
+              text-align: center;
+            }
+          </style>
+    </head>
+    <body>
+        <br>
+        <table align="center" width="90%" >
+            <tbody>
+                <tr>
+                    <td colspan="2">홍길동 육상 종목</td>
+                </tr>
+                <tr>
+                    <td><a href="../event/track.html" target=smj_board1>트랙종목</a></td>
+                    <td><a href="../event/field.html" target=smj_board2>필드종목</a></td>
+                </tr>
+            </tbody>
+        </table>
+        <br>
+        <iframe src="../event/iframe/Iframe1.html" align="left"
+        name="smj_board1" width="49%" height="200px"></iframe>
+        <iframe src="../event/iframe/Iframe2.html" align="right"
+        name="smj_board2" width="49%" height="200px"></iframe>
+        <iframe src="./bottom.html" align="bottom"
+        title="bottom" width="100%" height="93px"></iframe>
+    </body>
+</html>*/
